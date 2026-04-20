54faf517c713cf91115187a276e162e68841f643
keil101er <19856411032@163.com>
2026-04-20 21:04:40 +0800

    自瞄旋转靶参数

--- application/gimbal_task.h
+++ application/gimbal_task.h
@@ -51,7 +51,7 @@
 // #define YAW_SPEED_PID_KD        0.00f
 // #define YAW_SPEED_PID_MAX_OUT   4.0f
 // #define YAW_SPEED_PID_MAX_IOUT  1.0f   
-#define YAW_SPEED_PID_KP        1.5f
+#define YAW_SPEED_PID_KP        1.55f
 #define YAW_SPEED_PID_KI        0.00f
 #define YAW_SPEED_PID_KD        0.00f
 #define YAW_SPEED_PID_MAX_OUT   5.0f
@@ -111,11 +111,11 @@
 //yaw gyro angle close-loop PID params, max out and max iout
 //yaw 角度环 角度由陀螺仪解算 PID参数以及 PID最大输出，积分输出
 // #define YAW_AUTO_LOW_ABSOLUTE_PID_KP       28.0f//60
-#define YAW_AUTO_LOW_ABSOLUTE_PID_KP       10.0f//60
+#define YAW_AUTO_LOW_ABSOLUTE_PID_KP       14.0f//60
 // #define YAW_AUTO_LOW_ABSOLUTE_PID_KP       15.0f//60
 #define YAW_AUTO_LOW_ABSOLUTE_PID_KI       0.0f
 // #define YAW_AUTO_LOW_ABSOLUTE_PID_KD       -1.0f
-#define YAW_AUTO_LOW_ABSOLUTE_PID_KD       -0.2f
+#define YAW_AUTO_LOW_ABSOLUTE_PID_KD       -0.1f
 #define YAW_AUTO_LOW_ABSOLUTE_PID_MAX_OUT   10.0f
 #define YAW_AUTO_LOW_ABSOLUTE_PID_MAX_IOUT  0.0f
 
